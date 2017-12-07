d87441ed17834b4bf1ea891a3e1573b380e220c6
s4mr4t <rohit.ashiwal265@gmail.com>
2017-12-07 11:52:50 +0530

    canvas ready

--- app/src/main/java/com/rohit/kt/android_photo_editor/canvasActivity.java
+++ app/src/main/java/com/rohit/kt/android_photo_editor/canvasActivity.java
@@ -3,13 +3,11 @@ package com.rohit.kt.android_photo_editor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class canvasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_canvas);
     }
-
-
 }
