921d71dc85b0fdec3f8dd7441159fb84f72dd560
s4mr4t <rohit.ashiwal265@gmail.com>
2017-12-07 15:27:45 +0530

    camera activity added

--- app/src/main/java/com/rohit/kt/android_photo_editor/MainActivity.java
+++ app/src/main/java/com/rohit/kt/android_photo_editor/MainActivity.java
@@ -1,12 +1,14 @@
 package com.rohit.kt.android_photo_editor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button canvasButtton;
+    private Button canvasButton;
     private Button cameraButton;
 
     @Override
@@ -14,9 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        canvasButtton = findViewById(R.id.canBtn);
+        canvasButton = findViewById(R.id.canBtn);
         cameraButton = findViewById(R.id.camBtn);
+
+        canvasButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,canvasActivity.class));
+            }
+        });
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, cameraActivity.class));
+            }
+        });
     }
-
-
 }
