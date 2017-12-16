d34b9f11d3b11bc27f40dd4a1271c24e9169104a
s4mr4t <rohit.ashiwal265@gmail.com>
2017-12-16 12:30:54 +0530

    Dynamic permissions!

--- app/src/main/java/com/rohit/kt/android_photo_editor/MainActivity.java
+++ app/src/main/java/com/rohit/kt/android_photo_editor/MainActivity.java
@@ -1,6 +1,8 @@
 package com.rohit.kt.android_photo_editor;
 
+import android.Manifest;
 import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,11 +12,14 @@ public class MainActivity extends AppCompatActivity {
 
     private Button canvasButton;
     private Button cameraButton;
+    private final int REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, REQUEST_CODE);
 
         canvasButton = findViewById(R.id.canBtn);
         cameraButton = findViewById(R.id.camBtn);
