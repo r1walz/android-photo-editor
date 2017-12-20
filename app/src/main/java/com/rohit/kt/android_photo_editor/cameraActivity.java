6bb53f336e96a601e1c93169d21bb13584635cfd
s4mr4t <rohit.ashiwal265@gmail.com>
2017-12-20 11:34:49 +0530

    Added functionality for high res photo

--- app/src/main/java/com/rohit/kt/android_photo_editor/cameraActivity.java
+++ app/src/main/java/com/rohit/kt/android_photo_editor/cameraActivity.java
@@ -2,18 +2,25 @@ package com.rohit.kt.android_photo_editor;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.io.File;
+import java.io.IOException;
 
 public class cameraActivity extends AppCompatActivity {
 
     public static int k = 0;
     //TODO: add variables
     private Intent cameraIntent;
-    public  static  Bitmap image;
+    public static Bitmap image;
     private final int CAMERA_REQUEST_CODE = 1024;
+    private File imageFile;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +28,26 @@ public class cameraActivity extends AppCompatActivity {
         setContentView(R.layout.activity_camera);
 
         canvasActivity.drawView = findViewById(R.id.drawing);
+
+        imageFile = new File(Environment.getExternalStorageDirectory(), "IMG_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+        imageUri = Uri.fromFile(imageFile);
+
         cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+
         startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
-            image = (Bitmap) data.getExtras().get("data");
-            k=1;
-            startActivity(new Intent(cameraActivity.this, canvasActivity.class));
+            try {
+                image = MediaStore.Images.Media.getBitmap(getContentResolver(),imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            k = 1;
+            finish();
         }
     }
 }
