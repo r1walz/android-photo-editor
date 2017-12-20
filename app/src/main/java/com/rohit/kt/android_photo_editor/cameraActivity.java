1fa856d3109f50656c5ef0f62fd58ddc004f628e
s4mr4t <rohit.ashiwal265@gmail.com>
2017-12-20 14:20:04 +0530

    bug fixes

--- app/src/main/java/com/rohit/kt/android_photo_editor/cameraActivity.java
+++ app/src/main/java/com/rohit/kt/android_photo_editor/cameraActivity.java
@@ -8,8 +8,6 @@ import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
 import java.io.File;
 import java.io.IOException;
